a7b0f536ab96fbc58e6fd7ae2cf011bfa7f7a6cd
Fabricio <fabricio.bizotto@ifc.edu.br>
2025-10-21 17:10:18 -0300

    asd

--- src/components/RadioPlayer.jsx
+++ src/components/RadioPlayer.jsx
@@ -12,6 +12,7 @@ export default function RadioPlayer({ id }) {
                     src={EMBED_URL}
                     width="100%"
                     height="320"
+                    scrolling="no"
                     sandbox="allow-same-origin allow-scripts allow-popups allow-forms allow-presentation"
                     allow="autoplay; encrypted-media"
                     style={{
